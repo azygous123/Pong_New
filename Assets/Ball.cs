d582365e5638fb8d0c256b992200a01a0119514b
azygous123 <good7633@vandals.uidaho.edu>
2026-01-15 18:14:09 -0800

    getting further

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float startingSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bool isRight = UnityEngine.Random.value >= 0.5;
+
+        float xVelocity = -1f;
+
+        if (isRight)
+        {
+            xVelocity = 1f;
+        }
+
+        float yVelocity = UnityEngine.Random.Range(-1, 1);
+
+        rb.linearVelocity = new Vector2(xVelocity * startingSpeed, yVelocity * startingSpeed * 0);
     }
 
     // Update is called once per frame
